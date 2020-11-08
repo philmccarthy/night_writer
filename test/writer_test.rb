20be49c5fe3a75d15b6e3d4223c278d99ded222e
Phil McCarthy <philmcc2631@gmail.com>
2020-11-08 16:00:22 -0700

    Update writer test with dictionary in require

--- test/writer_test.rb
+++ test/writer_test.rb
@@ -1,15 +1,16 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/night_writer'
+require './lib/writer'
+require './lib/dictionary'
 require './lib/file_manager'
 
 class WriterTest < Minitest::Test
   def setup
-    @night_writer = NightWriter.new
+    @writer = Writer.new
   end
 
   def test_it_exists_and_has_attributes
-    assert_instance_of NightWriter, @night_writer
-    assert_instance_of FileManager, @night_writer.file_manager
+    assert_instance_of Writer, @writer
+    assert_instance_of FileManager, @writer.file_manager
   end
 end
