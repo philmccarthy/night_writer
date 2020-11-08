b29937f341b3d17fd1a01f5672598868956abc36
Phil McCarthy <philmcc2631@gmail.com>
2020-11-08 16:43:45 -0700

    Add test cases for encode_to_braille

--- test/writer_test.rb
+++ test/writer_test.rb
@@ -13,4 +13,13 @@ class WriterTest < Minitest::Test
     assert_instance_of Writer, @writer
     assert_instance_of FileManager, @writer.file_manager
   end
+
+  def test_it_can_convert_single_letter_to_braille
+    assert_equal "0.\n00\n..", @writer.encode_to_braille(['h'])
+  end
+
+  def test_it_can_convert_hello_world_to_braille
+    assert_equal "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...",
+    @writer.encode_to_braille(["hello world"])
+  end
 end
