0259e88275c1ff0736c5f9c546d0ae47bc987495
Phil McCarthy <philmcc2631@gmail.com>
2020-11-08 17:24:28 -0700

    Update file_manager test case for read

--- test/file_manager_test.rb
+++ test/file_manager_test.rb
@@ -1,16 +1,15 @@
-require 'minitest/autorun'
-require 'minitest/pride'
+require './test/test_helper.rb'
 require './lib/file_manager'
 
 class FileManagerTest < Minitest::Test
   def test_it_exists_and_has_attributes
-    file_manager = FileManager.new
+    file_manager = FileManager.new('writer')
     assert_instance_of FileManager, file_manager
   end
 
-  def test_it_responds_to_read_and_write
-    file_manager = FileManager.new
-    assert_respond_to file_manager, :read
-    assert_respond_to file_manager, :write
+  def test_it_reads_text_from_file
+    file_manager = FileManager.new('writer')
+    File.stubs(:readlines).returns("some text")
+    assert_equal "some text", file_manager.read
   end
 end
