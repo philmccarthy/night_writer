2de65f1e04942480f4ccb27f769882bf23e3d9f1
Phil McCarthy <philmcc2631@gmail.com>
2020-11-09 12:35:50 -0700

    Update logger test case to include instance_of assertion

--- test/logger_test.rb
+++ test/logger_test.rb
@@ -4,7 +4,10 @@ require './lib/logger'
 class LoggerTest < Minitest::Test
   def test_it_exists_and_can_print_confirmation
     logger = Logger.new
-    logger.stubs(:puts).returns("Created text.txt containing 11 characters")
-    assert_equal "Created text.txt containing 11 characters", logger.confirmation_msg('text.txt', 11)
+    assert_instance_of Logger, logger
+    path = 'test.txt'
+    count = 11
+    logger.stubs(:puts).returns("Created #{path} containing #{11} characters")
+    assert_equal "Created test.txt containing 11 characters", logger.confirmation_msg(path, count)
   end
 end
