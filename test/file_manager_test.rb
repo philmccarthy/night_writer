ac475e38e5cadb06a7d23281b85dba1fd6bb7709
Phil McCarthy <philmcc2631@gmail.com>
2020-11-08 18:29:03 -0700

    Add test for write method

--- test/file_manager_test.rb
+++ test/file_manager_test.rb
@@ -12,4 +12,15 @@ class FileManagerTest < Minitest::Test
     File.stubs(:readlines).returns("some text")
     assert_equal "some text", file_manager.read
   end
+
+  def test_it_writes_to_file
+    file_manager = FileManager.new('writer')
+    ARGV.replace ["test_in.txt", "test_out.txt"]
+    assert_equal "test_out.txt", file_manager.write('0.0.0.').path
+  end
+
+  def test_it_can_count_input_characters
+    file_manager = FileManager.new('writer')
+    assert_equal 11, file_manager.count_characters(["hello world\n"])
+  end
 end
