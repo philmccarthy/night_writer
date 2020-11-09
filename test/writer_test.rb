16350536869d3f3813c6bafe98e3d8b3b18c8609
Phil McCarthy <philmcc2631@gmail.com>
2020-11-08 17:57:58 -0700

    Add multiline test for encode method

--- test/writer_test.rb
+++ test/writer_test.rb
@@ -1,5 +1,4 @@
-require 'minitest/autorun'
-require 'minitest/pride'
+require './test/test_helper.rb'
 require './lib/writer'
 require './lib/dictionary'
 require './lib/file_manager'
@@ -19,7 +18,11 @@ class WriterTest < Minitest::Test
   end
 
   def test_it_can_convert_hello_world_to_braille
-    assert_equal "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...",
-    @writer.encode_to_braille(["hello world"])
+    assert_equal "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...", @writer.encode_to_braille(["hello world"])
+  end
+
+  def test_it_can_output_multiline_braille
+    expected = "0.0.0.0.0....00.0.0.00..0.0.0.0.0....00.0.0.00..0.0.0.0.0....00.0.0.00..0.0.0.0.\n00.00.0..0..00.0000..0..00.00.0..0..00.0000..0..00.00.0..0..00.0000..0..00.00.0.\n....0.0.0....00.0.0.........0.0.0....00.0.0.........0.0.0....00.0.0.........0.0.\n0....00.0.0.00..0.0.0.0.0....00.0.0.00..0.0.0.0.0....00.0.0.00\n.0..00.0000..0..00.00.0..0..00.0000..0..00.00.0..0..00.0000..0\n0....00.0.0.........0.0.0....00.0.0.........0.0.0....00.0.0...\n"
+    assert_equal expected, @writer.encode_to_braille(["hello world hello world hello world hello world hello world hello world"])
   end
 end
