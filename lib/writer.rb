cd739df660d2fe2aa5d08a733802d68c6e471683
Phil McCarthy <philmcc2631@gmail.com>
2020-11-09 17:30:31 -0700

    Fix indentation

--- lib/writer.rb
+++ lib/writer.rb
@@ -14,9 +14,9 @@ class Writer
   end
 
   def encode_file_to_braille
-      incoming_text = file_manager.read
-      encoded = encode_to_braille(incoming_text)
-      file_manager.write(encoded)
+    incoming_text = file_manager.read
+    encoded = encode_to_braille(incoming_text)
+    file_manager.write(encoded)
   end
 
   def encode_to_braille(input)
